7e3cbdabbb5623facfc3bc8ac17b3b174ab19a51
Reid Q <92822618+RAQuig@users.noreply.github.com>
2026-02-17 13:46:40 -0600

    added (POTENTIALLY) Stable QMK firmware files

---
+++ config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define VENDOR_ID 0xFEED
+#define PRODUCT_ID 0x1234
+#define DEVICE_VER 0x0100
+#define MANUFACTURER srldesignationr
+#define PRODUCT hackpad
+
+#define MATRIX_ROWS 1
+#define MATRIX_COLS 3
+#define DIODE_DIRECTION COL2ROW
+
+#define DEBOUNCE 5
